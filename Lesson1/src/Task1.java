b7b507f5c18bed1f8fa304f10fb4cd569d3b998f
bearmaaxx7377 <Bear_maxx_7377>
2021-09-29 20:30:30 +0300

    Решение задач

--- Lesson1/src/Task1.java
+++ Lesson1/src/Task1.java
@@ -1,6 +1,12 @@
 public class Task1 {
     public static void main(String[] args) {
        int count = 0;
-
+        for (String arg : args) {
+            int i = Integer.parseInt(arg);
+            if (i > 0) {
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
     }
 }
