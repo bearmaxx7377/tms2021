6abe21035be8973c258bff8327a3746a65fe1339
bearmaaxx7377 <Bear_maxx_7377>
2021-10-03 22:20:58 +0300

    HomeWork1

---
+++ Lesson1_1/src/main/java/HomeWork1.java
@@ -0,0 +1,80 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        //Некоторые тесты для проверки задач.
+        System.out.println(sum(5, 10));
+        System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        System.out.println(max(7, 10));
+        System.out.println(average(new int[]{0, -2, 3, -1, 5}));
+        System.out.println(max(new int[]{1, 2, 3, 4, 5, 87, 99}));
+        System.out.println(calculateHypotenuse(12,16));
+    }
+
+    /**
+     * 1. Метод должен вернуть сумму двух чисел a и b
+     * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
+     **/
+    public static int sum(int a, int b) {
+      return a+b;
+    }
+
+    /**
+     *
+     * Метод должен вернуть максимальное значение из двух чисел
+     *
+     * <p>
+     * Example1:
+     * a = 4,
+     * b = 5
+     * <p>
+     * Метод должен вернуть 5
+     * Example2:
+     * a = 10,
+     * b = 10
+     * <p>
+     * Метод должен вернуть 10
+     */
+    public static int max(int a1, int b1) {
+        return (a1 > b1) ? a1 : b1;
+
+
+    }
+
+    /**
+     * Метод должен вернуть среднее значение из массива чисел
+     * (необходимо сумму всех элеменов массива разделить на длину массива)
+     * <p>
+     * Example:
+     * array = {1,2,3,4,5}
+     * Метод должен return 3.0
+     */
+    public static double average(int[] array) {
+       return 0;
+    }
+
+    /**
+     * Метод должен вернуть максимальый элемент массива. Пример: array = {1,2,10,3} метод возвращает 10
+     **/
+    public static int max(int[] array) {
+        int maximum = array[0];
+        for (int i = 0; i < array.length; i++)
+            if (maximum < array[i]) maximum = array[i];
+        return maximum;
+    }
+
+    /**
+     * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
+     *
+     * Example1:
+     * 3
+     * 4
+     * return 5
+     * <p>
+     * Example2:
+     * 12
+     * 16
+     * return 20
+     */
+    public static double calculateHypotenuse(int a3, int b3) {
+       return Math.sqrt(a3*a3 + b3*b3);
+    }
+}
