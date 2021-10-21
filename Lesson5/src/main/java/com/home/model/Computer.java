becb68734942642f286b73814eb4a44908a10735
bearmaaxx7377 <Bear_maxx_7377>
2021-10-21 14:34:15 +0300

    Lesson5 - исправление ошибок

--- Lesson5/src/main/java/com/home/model/Computer.java
+++ Lesson5/src/main/java/com/home/model/Computer.java
@@ -1,12 +1,57 @@
 package com.home.model;
 
-public class Computer {
-   public String processor;
-   public int ram;
-   public int hardDrive;
-   public String resurs;
+import java.util.Random;
+import java.util.Scanner;
 
-   public Computer() {
-      System.out.println("Вы попали в конструктор 'Computer'");
+public class Computer {
+   private String processor;
+   private int ram;
+   private int hardDrive;
+   private int resurs;
+   private Scanner scanner = new Scanner(System.in);
+   private Random random = new Random();
+   private boolean burned; // true или false (2 состояния)
+
+   public Computer(String processor, int ram, int hardDrive, int resurs) {
+      this.processor = processor;
+      this.ram = ram;
+      this.hardDrive = hardDrive;
+      this.resurs = resurs;
+   }
+
+   public String info() {
+      return "Процессор: " + processor + " Оперативка: " + ram + " Жёсткий диск: " + hardDrive + " Ресурсы: " + resurs;
+   }
+
+   public void info2() {
+      System.out.println("Процессор: " + processor + " Оперативка: " + ram + " Жёсткий диск: " + hardDrive + " Ресурсы: " + resurs);
+   }
+
+   public void on() {
+      if (!burned) {
+         System.out.println("Включение компьютера...");
+         if (resurs > 0 && scanner.nextInt() == random.nextInt(2)) {
+            off();
+         } else {
+            burned();
+         }
+      } else {
+         burned();
+      }
+   }
+
+   private void burned() {
+      System.out.println("Компьютер сгорел!!!");
+      burned = true;
+   }
+
+   public void off() {
+      if (!burned) {
+         System.out.println("Выключение компьютера...");
+         resurs--;
+         on();
+      } else {
+         burned();
+      }
    }
 }
--- Lesson5/src/main/java/com/home/service/Main.java
+++ Lesson5/src/main/java/com/home/service/Main.java
@@ -4,16 +4,13 @@ import com.home.model.Computer;
 
 public class Main {
     public static void main(String[] args) {
-        Computer comp1 = new Computer();
-        comp1.processor = "ATI";
-        comp1.ram = 6666;
-        comp1.hardDrive = 10000;
+        Computer computer = new Computer("AMD", 1024, 5000, 5);
+        String info = computer.info();
+        System.out.println(info);
 
-        print(comp1);
-
-    }
-
-    private static void print(Computer comp1) {
-        System.out.println(comp1.hardDrive + " " + comp1.processor + " " + comp1.ram);
+        computer.on();
+        computer.off();
+        computer.off();
+        computer.on();
     }
 }
