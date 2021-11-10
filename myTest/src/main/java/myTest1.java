f6f19668a22b7c6aa62a79561c5e7c3d1e9bf3ca
bearmaxx7377 <maksim.drozdovw@gmail.com>
2021-11-10 16:29:30 +0300

    myTest

--- myTest/src/main/java/myTest1.java
+++ myTest/src/main/java/myTest1.java
@@ -1,20 +1,20 @@
 public class myTest1 {
     public static void main(String[] args) {
         //public String getDayAddition(int num) {
-        int num;
-        int preLastDigit = num % 100 / 10;
-        if (preLastDigit == 1) {
-            return "дней";
-        }
-        switch (num % 10) {
-            case 1:
-                return "день";
-            case 2:
-            case 3:
-            case 4:
-                return "дня";
-            default:
-                return "дней";
-        }
+//        int num;
+//        int preLastDigit = num % 100 / 10;
+//        if (preLastDigit == 1) {
+//            return "дней";
+//        }
+//        switch (num % 10) {
+//            case 1:
+//                return "день";
+//            case 2:
+//            case 3:
+//            case 4:
+//                return "дня";
+//            default:
+//                return "дней";
+//        }
     }
 }
